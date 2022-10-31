4d9ef0747213335355ee69a3cddc7e24fd6d81b5
Andrew Cox <18695702+Cox-Andrew@users.noreply.github.com>
2022-10-31 15:23:04 +0000

    messy but works :)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pushbullet import Pushbullet
 import sys
 import pickle
 from os.path import exists
+from os import mkdir
 
 import constants
 
@@ -27,19 +28,42 @@ if __name__ == '__main__':
   user_response = twt_client.get_user(username=sys.argv[1])
   error_check(user_response)
 
-  cache_path = f'./history/{user_response.data.id}'
-  if (not exists(cache_path)):
-    with open(cache_path, mode='r') as f:
-      
+  if (not exists('./history')): mkdir('./history')
+
+  cache_path = f'./history/{user_response.data.id}.pickle'
+  history = set()
+
+  new = not exists(cache_path)
+
+  liked_twts = twt_client.get_liked_tweets(user_response.data.id, max_results=(100 if new else 5))
+  error_check(liked_twts)
+  print(f'Fetched {len(liked_twts.data)} recently liked tweets')
+  
+  if (new):
+    history = set(map(lambda x: x.id, liked_twts.data))
+    with open(cache_path, 'wb') as f:
+      pickle.dump(history, f)
 
     print(f'New user @{user_response.data.username}\'s like history stored. Check back later.')
     exit()
+  
+  with open(cache_path, 'rb') as f:
+    history = pickle.load(f)
 
-  liked_twts = twt_client.get_liked_tweets(user_response.data.id)
-  error_check(liked_twts)
-  print(liked_twts.data)
+  if liked_twts.data[0].id not in history:
+    # new liked tweet detected
+    print(f'@{user_response.data.username} liked a new tweet', f'https://twitter.com/{user_response.data.username}/likes')
 
-  if (len(liked_twts.data) == 0): exit()
+    # auth PB
+    try:
+      pb = Pushbullet(constants.PUSHBULLET_KEY)
+      pb.push_link(f'@{user_response.data.username} liked a new tweet', f'https://twitter.com/{user_response.data.username}/likes')
+      print('Pushbullet notified')
+    except:
+      print('Failed to send PushBullet notification')
 
-  for twt in liked_twts.data:
-    
+    history.add(liked_twts.data[0].id)
+    with open(cache_path, 'wb') as f:
+      pickle.dump(history, f)
+  else:
+    print('No new likes found. Have a nice day :)')
